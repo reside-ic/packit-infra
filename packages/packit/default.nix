5aea73848bc3df367120aa52cb5500c253e8d400
Paul Lietar <pl2113@ic.ac.uk>
2024-08-27 12:35:45 +0100

    Update Packit source.
    Packit now has a functional package-lock.json file, which removes the
    need for us to vendor a fixed up version of it here.

--- packages/packit/default.nix
+++ packages/packit/default.nix
@@ -7,13 +7,8 @@
 buildNpmPackage rec {
   name = "packit-app";
   src = fetchFromGitHub (lib.importJSON ./sources.json);
-  npmDepsHash = "sha256-LiqPRDW/CulR6GN84v1VrbN4q4Qw0zcbJ+rzAk0cSOc=";
+  npmDepsHash = "sha256-o//+q3trkCnKpSAWEHPZOeiMYxzKOvrGDgEv63BsnxI=";
   sourceRoot = "${src.name}/app";
-
-  # For some reason Packit is missing integrity and resolved fields
-  postPatch = ''
-    cp ${./package-lock.json} ./package-lock.json
-  '';
 
   installPhase = ''
     runHook preInstall
