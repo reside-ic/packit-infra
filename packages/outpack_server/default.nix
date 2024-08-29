3c864e9650d1e1aa9e3df848a83d6e0972b05e26
Paul Lietar <pl2113@ic.ac.uk>
2024-08-29 13:51:19 +0100

    WIP

--- packages/outpack_server/default.nix
+++ packages/outpack_server/default.nix
@@ -1,9 +1,11 @@
 { rustPlatform, fetchFromGitHub, lib, openssl, pkg-config }:
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage rec {
   name = "outpack_server";
   src = fetchFromGitHub (lib.importJSON ./sources.json);
   cargoHash = "sha256-4Jf5L+JUetHqYslVpHQOyXEpZvAIE5zD3f+2xeGhkoU=";
 
   buildInputs = [ openssl ];
   nativeBuildInputs = [ pkg-config ];
+
+  VERGEN_GIT_SHA = src.rev;
 }
