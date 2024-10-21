749322de0fd01681238996b3a0b0bc8eac97d74c
Mantra <mantrakusumgar@outlook.com>
2024-10-21 16:07:06 +0100

    Add Private Packit\nThis PR adds the machines folder to allow for different configurations per machine and adds private counterparts to deploy and start-vm scripts for our internal multi-packit instance

---
+++ machines/common/configuration.nix
@@ -0,0 +1,49 @@
+{ lib, pkgs, inputs, ... }: {
+  imports = [
+    ../../disk-config.nix
+    ../../tools.nix
+    ../../modules/multi-packit.nix
+    ../../modules/vault.nix
+    ../../modules/outpack.nix
+    ../../modules/packit-api.nix
+    ../../modules/metrics-proxy.nix
+    ./services.nix
+
+    inputs.disko.nixosModules.disko
+  ];
+
+  virtualisation.vmVariant = {
+    imports = [ ../../vm.nix ];
+  };
+
+  boot.loader.grub = {
+    # no need to set devices, disko will add all devices that have a EF02 partition to the list already
+    # devices = [ ];
+    efiSupport = true;
+    efiInstallAsRemovable = true;
+  };
+
+  networking.firewall.enable = true;
+  networking.firewall.allowedTCPPorts = [ 22 80 443 9000 ];
+
+  environment.systemPackages = [
+    pkgs.curl
+    pkgs.htop
+    pkgs.vim
+    pkgs.outpack_server
+    pkgs.gitMinimal
+  ];
+
+  users.users.root.openssh.authorizedKeys.keyFiles = [
+    ../../authorized_keys
+  ];
+
+  users.users.vscan-dide = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDG/MCRfHKu4trSoii5eeozpZlOQJ5t8YwowTBy5q5rDCUr/pcZNFmfzEfeTerOV2ON8CiAfx/LADCJFRNRvSiMawGqF6U3Xstk/JTh6IXbowukhdKqpqom/BF2Oryy6iDFlnUTX3wJJGdsV/9DnSBzudngsmSMOFs8aFbVKrQ3V6mI7itq2+Qfg4a428uU1912TG2n9SnEYTnufYtPIivz+Kx/3YP5o5u2YW9FgZJlp502ce4harols28wsmVd2jZ9yrlRGlQSyuRnSf45PKiVdv4CEhCb7ppSrE7u0lGnhT2uENm+jpHoJ4/CSxtrUgQFDCqJarQuipojhPurRqEXT+NX6wjgqOXof2ouBrVKvJ3NoSCuMAltPxDazC+UELy0y67uiAq5APnwzny7w+VxkbBz3b1tetI9igZ13AjhVu0R4SeSHjb/TZkYdx+kUHEzLzv1NgzeGCVY7NKjTOFnqqneIQIeOwW/0bxQ9FoPsz/u3D3OYwyjnhNG9GUySHk= ic/elton@icsecwop2" ];
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  system.stateVersion = "24.05";
+}
---
+++ machines/common/services.nix
@@ -0,0 +1,58 @@
+{ pkgs, config, lib, inputs, ... }:
+{
+  services.nginx.enable = true;
+  services.openssh.enable = true;
+  services.openssh.settings.HostKeyAlgorithms = lib.concatStringsSep "," [
+    "rsa-sha2-512"
+    "rsa-sha2-256"
+    "ssh-ed25519"
+    "ssh-rsa" # For ICT
+  ];
+  services.openssh.settings.Macs = [
+    "hmac-sha2-512-etm@openssh.com"
+    "hmac-sha2-256-etm@openssh.com"
+    "umac-128-etm@openssh.com"
+    "hmac-sha2-256" # For ICT
+    "hmac-sha2-512" # For ICT
+  ];
+
+  services.postgresql = {
+    enable = true;
+    ensureUsers = [{
+      name = "root";
+      ensureClauses.superuser = true;
+      ensureClauses.login = true;
+    }];
+    authentication = ''
+      local all      all                    trust
+      host  all      all     127.0.0.1/32   trust
+      host  all      all     ::1/128        trust
+    '';
+  };
+
+  services.metrics-proxy = {
+    enable = true;
+    domain = config.services.multi-packit.domain;
+    endpoints."node_exporter" = {
+      upstream = "http://localhost:${toString config.services.prometheus.exporters.node.port}/metrics";
+      labels.job = "machine-metrics";
+    };
+  };
+
+  services.prometheus.exporters = {
+    node =
+      let
+        inherit (inputs) self;
+        rev = self.rev or self.dirtyRev or "unknown";
+        staticMetrics = pkgs.writeTextDir "static-metrics.prom" ''
+          nixos_configuration_info{revision="${rev}", flake_hash="${self.narHash}"} 1
+        '';
+      in
+      {
+        enable = true;
+        enabledCollectors = [ "systemd" "textfile" ];
+        extraFlags = [ "--collector.textfile.directory=${staticMetrics}" ];
+        port = 9001;
+      };
+  };
+}
