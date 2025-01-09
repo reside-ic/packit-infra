68fa39e8f621d9c951735a2b1c7ca2de12720d9c
Paul Lietar <pl2113@ic.ac.uk>
2025-01-09 14:23:22 +0000

    Add comments on gradle magic.

--- packages/packit/packit-api.nix
+++ packages/packit/packit-api.nix
@@ -35,7 +35,10 @@ let
 
       nativeBuildInputs = [ gradle ] ++ nativeBuildInputs;
 
+      # The default gradle configure forces --offline, which we don't want for
+      # our deps fetching phase. We do our own configure instead.
       dontUseGradleConfigure = true;
+
       gradleFlags = gradleFlags ++ [
         "--no-daemon"
         "--console=plain"
@@ -53,6 +56,20 @@ let
   deps = makePackage {
     name = "packit-api-deps";
     nativeBuildInputs = [ perl ];
+
+    # This slight insanity searches the gradle cache and transforms it into a
+    # file layout that looks like a Maven repository. The repository we create
+    # can then be used as an input to the real build.
+    #
+    # We need the sort to keep things deterministic: the cache may contain the
+    # same file name twice with different hashes, as long as we pick a winner
+    # deterministically things are generally okay.
+    #
+    # The perl command transforms the paths, turning the following
+    # /path/to/cache/org.springframework/spring-beans/6.0.4/30ee07d3fcc6bbf13ab4ddca2dff9ebad55dee51/spring-beans-6.0.4.pom
+    # into
+    # $out/org/springframework/spring-beans/6.0.4/spring-beans-6.0.4.pom
+    #
     installPhase = ''
       find $GRADLE_USER_HOME/caches/modules-2 -type f -regex '.*\.\(jar\|pom\|module\)' \
          | LC_ALL=C sort \
@@ -60,6 +77,7 @@ let
          | sh
       rm -rf $out/tmp
     '';
+
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
     outputHash = sources.gradleDepsHash;
